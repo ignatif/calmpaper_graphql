33ee095afe3e39a37527d7290fc95615d5dbc4a8
Ubuntu <ubuntu@ip-172-31-56-143.ec2.internal>
2020-08-20 13:51:46 +0000

    .

--- prisma/genres_seed.js
+++ prisma/genres_seed.js
@@ -3,6 +3,7 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 const genres = [
+<<<<<<< HEAD
   'Fiction',
   'Action and adventure',
   'Art/architecture',
@@ -60,6 +61,98 @@ const genres = [
   'Travel',
   'Young adult',
   'True crime',
+=======
+  'Cookbook',
+
+  'Comic book',
+
+  'Diary',
+
+  'Coming-of-age',
+
+  'Dictionary',
+
+  'Crime',
+
+  'Encyclopedia',
+
+  'Drama',
+
+  'Guide',
+
+  'Fairytale',
+
+  'Health/fitness',
+
+  'Fantasy',
+
+  'History',
+
+  'Graphic novel',
+
+  'Home and garden',
+
+  'Historical fiction',
+
+  'Humor',
+
+  'Horror',
+
+  'Journal',
+
+  'Mystery',
+
+  'Math',
+
+  'Paranormal romance',
+
+  'Memoir',
+
+  'Picture book',
+
+  'Philosophy',
+
+  'Poetry',
+
+  'Prayer',
+
+  'Political thriller',
+
+  'Religion, spirituality, and new age',
+
+  'Romance',
+
+  'Textbook',
+
+  'Satire',
+
+  'True crime',
+
+  'Science fiction',
+
+  'Review',
+
+  'Short story',
+
+  'Science',
+
+  'Suspense',
+
+  'Self help',
+
+  'Thriller',
+
+  'Sports and leisure',
+
+  'Western',
+
+  'Travel',
+
+  'Young adult',
+
+  'True crime',
+  ,
+>>>>>>> 0211aec... m
 ]
 
 async function main() {
@@ -69,7 +162,11 @@ async function main() {
         label: genre,
       },
     })
+<<<<<<< HEAD
     console.log('seed', genre)
+=======
+    console.log(genre, 'seed')
+>>>>>>> 0211aec... m
   })
 }
 
@@ -80,3 +177,7 @@ main()
   .finally(async () => {
     await prisma.disconnect()
   })
+<<<<<<< HEAD
+=======
+
+>>>>>>> 0211aec... m
