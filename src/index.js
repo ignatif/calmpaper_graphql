b62f682e228f81a57e9d9364953bfac71645b8f7
jjjuk <gmodhl67@gmail.com>
2020-11-12 14:02:51 +0800

    bodyReplaceOldPoll field for comments (not for client)

--- src/index.js
+++ src/index.js
@@ -257,7 +257,7 @@ server.express.get(
   },
 )
 
-server.start({ port: 3000 },() =>
+server.start({ port: 4000 },() =>
   console.log(
     `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/js/graphql#using-the-graphql-api`,
   ),
